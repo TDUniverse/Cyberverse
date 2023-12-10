3c420cb09bcf25f573484d791e052e9b88d234d9
MeFisto94 <MeFisto94@users.noreply.github.com>
2023-12-10 14:17:35 +0100

    Server Tickrate 100->10

--- Native/src/GameServer.cpp
+++ Native/src/GameServer.cpp
@@ -387,7 +387,7 @@ void GameServer::RunBlocking()
         PollIncomingMessages();
         PollConnectionStateChanges();
         //PollLocalUserInput();
-        std::this_thread::sleep_for( std::chrono::milliseconds( 10 ) );
+        std::this_thread::sleep_for( std::chrono::milliseconds( 100 ) );
     }
 }
 void GameServer::Destroy()
